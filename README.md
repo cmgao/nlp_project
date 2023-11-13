fd8484fe86d73ad81356915b2286b4fda49efde4
SuperAIdesu <tjx10011@gmail.com>
2023-11-13 15:55:36 -0500

    evaluation script

--- README.md
+++ README.md
@@ -21,3 +21,26 @@ When installing new packages to the virtual environment, first add to `pyproject
 ```
 poetry lock
 poetry install --no-root
+```
+
+## Evaluation script and results
+
+Use `evaluate.py -h` to see the options. The script takes in a plain text output file, whose rows are the model translations, and output BLEU and ChrF++ scores.
+
+We have 2 directions(Mandarin-Cantonese, Cantonese-Mandarin), 2 data sources(Main, Tatoeba), 2 splits(validation, test), 2 metrics, so 16 values for each method.
+
+Mandarin to Cantonese:
+
+| Method(validation/test) | BLEU(main)    | BLEU(tatoeba) | ChrF++(main)  | ChrF++(tatoeba) |
+| ----------------------- | ------------- | ------------- | ------------- | --------------- |
+| Naive baseline          | 12.356/13.103 | 21.987/24.761 | 10.872/11.373 | 16.195/16.645   |
+| Existing work           |               |               |               |                 |
+| ---                     |               |               |               |                 |
+
+Cantonese to Mandarin:
+
+| Method(validation/test) | BLEU(main)    | BLEU(tatoeba) | ChrF++(main)  | ChrF++(tatoeba) |
+| ----------------------- | ------------- | ------------- | ------------- | --------------- |
+| Naive baseline          | 12.437/13.181 | 21.974/24.711 | 10.617/11.136 | 16.450/16.825   |
+| Existing work           |               |               |               |                 |
+| ---                     |               |               |               |                 |
---
+++ evaluate.py
@@ -0,0 +1,83 @@
+import argparse
+import pandas as pd
+from typing import Sequence, Callable
+from sacrebleu.metrics import BLEU, CHRF
+
+
+def evaluate(ref: Sequence[str], pred: Sequence[str]) -> dict:
+    """
+    Evaluate the predictions against the reference using BLEU and chrF++.
+    :param ref: list of reference sentences
+    :param pred: list of predicted sentences
+    :return: dictionary of scores
+    """
+    print("Reference:")
+    print(ref[:5])
+    print("Prediction:")
+    print(pred[:5])
+
+    bleu = BLEU(tokenize="zh")
+    chrf = CHRF(word_order=2)
+
+    bleu_score = bleu.corpus_score(pred, [ref]).score
+    chrf_score = chrf.corpus_score(pred, [ref]).score
+
+    return {"bleu": bleu_score, "chrf++": chrf_score}
+
+
+def dummy_pred(ref: pd.DataFrame, target: str) -> Sequence[str]:
+    """
+    Use source text as naive baseline.
+    """
+    return ref.iloc[:, 1].tolist() if target == "yue" else ref.iloc[:, 0].tolist()
+
+
+def evaluation_driver(input: str | Callable | None, split="val", domain="main", target="yue"):
+    """
+    :param input: If None, use source text as prediction.
+                If callable, use it to generate predictions. It should take a list of source text and target language specifier.
+                Otherwise, read from file.
+    :param split: test or val
+    :param domain: main or tatoeba
+    :param target: yue or cmn
+    """
+    if split == "test":
+        if domain == "main":
+            ref = pd.read_csv("data/test_cleaned_parallel_sentences.txt").iloc[:, -2:]
+        else:
+            ref = pd.read_csv("data/test_tatoeba_sentences.txt")
+    elif domain == "main":
+        ref = pd.read_csv("data/valid_cleaned_parallel_sentences.txt").iloc[:, -2:]
+    else:
+        ref = pd.read_csv("data/valid_tatoeba_sentences.txt")
+
+    if input is None:
+        pred = dummy_pred(ref, target)
+    elif callable(input):
+        pred_input = dummy_pred(ref, target)
+        pred = input(pred_input, target)
+    else:
+        with open(input, "r") as f:
+            pred = f.readlines()
+
+    ref_list = ref.iloc[:, 0].tolist() if target == "yue" else ref.iloc[:, 1].tolist()
+
+    scores = evaluate(ref_list, pred)
+
+    print(scores)
+
+    return scores
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--test', dest='split', action='store_const',
+                    const="test", default="val",
+                    help='Evaluate on test set instead of validation test')
+    parser.add_argument('--input', help='Path to input file (contains predictions)', default=None)
+    parser.add_argument("--domain", help='which data source to use (main or tatoeba)', choices=["main", "tatoeba"], required=True)
+    parser.add_argument('--target', help='Target language (yue or cmn)', choices=["yue", "cmn"], required=True)
+
+    args = parser.parse_args()
+
+    evaluation_driver(args.input, args.split, args.domain, args.target)
