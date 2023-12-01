7bf5691c2982c69566f9b3c57fb14ac32f7b4681
cmgao <gao.min2468@gmail.com>
2023-12-01 09:40:34 -0500

    Update prompting.py

--- prompting.py
+++ prompting.py
@@ -4,9 +4,8 @@ import pandas as pd
 import numpy as np
 import jieba
 from tqdm import tqdm
-import nltk
-from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
-from nltk.translate.chrf_score import sentence_chrf
+from typing import Sequence, Callable
+from sacrebleu.metrics import BLEU, CHRF
 
 def load_split(file_path):
     if 'parallel' in file_path.lower():
@@ -16,44 +15,37 @@ def load_split(file_path):
     return data
 
 
-# def tokenize_data(tokenizer, data):
-#     text2text_generator = pipeline("text-generation", model="indiejoseph/cantonese-llama-2-7b-oasst-v1")
+# def evaluate_bleu(predictions, ground_truth):
+#     # Add the prompt to the Cantonese sentences
+#     smooth_fn = SmoothingFunction()
 
-#     tokenized_mando = tokenizer(data['mandarin'].tolist(), truncation=True, padding=True, max_length=256, return_tensors="pt").input_ids
+#     bleu_score = []
+#     chrf_score = []
 
-#     return tokenized_canto, tokenized_mando
+#     canto_list = [[list(jieba.cut(item.strip(), use_paddle=True, cut_all=False))]for item in predictions]
+#     mando_list = [list(jieba.cut(item.strip(), use_paddle=True, cut_all=False)) for item in ground_truth]
 
-def evaluate_bleu(predictions, ground_truth):
-    # Add the prompt to the Cantonese sentences
-    smooth_fn = SmoothingFunction()
+#     # print(len(canto_list), len(mando_list))
 
-    bleu_score = []
-    chrf_score = []
+#     for gd, zh in zip(canto_list, mando_list):
+#         s = nltk.translate.bleu_score.sentence_bleu(gd, zh, smoothing_function=smooth_fn.method3)
+#         bleu_score.append(s)
 
-    canto_list = [[list(jieba.cut(item.strip(), use_paddle=True, cut_all=False))]for item in predictions]
-    mando_list = [list(jieba.cut(item.strip(), use_paddle=True, cut_all=False)) for item in ground_truth]
+#         gd_str = " ".join([word for sublist in gd for word in sublist])
+#         zh_str = " ".join([word for sublist in zh for word in sublist])
 
-    # print(len(canto_list), len(mando_list))
+#         ## calculate chrff++ score
+#         c = nltk.translate.chrf_score.sentence_chrf(gd_str, zh_str, min_len=1, beta=2) 
+#         chrf_score.append(c)
 
-    for gd, zh in zip(canto_list, mando_list):
-        s = nltk.translate.bleu_score.sentence_bleu(gd, zh, smoothing_function=smooth_fn.method3)
-        bleu_score.append(s)
+#     bleu_score = np.mean(bleu_score) * 100.0
+#     chrf_score = np.mean(chrf_score) * 100.0
 
-        gd_str = " ".join([word for sublist in gd for word in sublist])
-        zh_str = " ".join([word for sublist in zh for word in sublist])
+#     # print('BLEU:', bleu_score)
+#     # bleu_score = nltk.translate.bleu_score.sentence_bleu(predictions, ground_truth) * 100.0
 
-        ## calculate chrff++ score
-        c = nltk.translate.chrf_score.sentence_chrf(gd_str, zh_str, min_len=1, beta=2) 
-        chrf_score.append(c)
-
-    bleu_score = np.mean(bleu_score) * 100.0
-    chrf_score = np.mean(chrf_score) * 100.0
-
-    # print('BLEU:', bleu_score)
-    # bleu_score = nltk.translate.bleu_score.sentence_bleu(predictions, ground_truth) * 100.0
-
-    print(f"SacreBLEU Score: {bleu_score}")
-    print(f"Chrff++ Score: {chrf_score}")
+#     print(f"SacreBLEU Score: {bleu_score}")
+#     print(f"Chrff++ Score: {chrf_score}")
 
 def main():
     import argparse
@@ -80,7 +72,7 @@ def main():
     ## few shot - input lang : sample sentence --> output lang : output sentence
     template_few_shot = f"""Translate the {args.input_lang} text into {args.output_lang}.
     {args.input_lang}: {{}}
-    {args.output_lang}: {{}}
+    {args.output_lang}: 
     """
 
     tokenizer.pad_token = "[PAD]"
@@ -92,34 +84,42 @@ def main():
         outputs = []
         total_batches = len(input_texts) // batch_size
 
-        for i in tqdm(range(0, len(input_texts), batch_size), total=total_batches, desc="Inference"):
-            batch_texts = input_texts[i:i+batch_size]
+        selected_samples = data.sample(n=5)
 
-            ## for few shot prompting
-            if use_few_shot_prompting:
-                prompt_texts = []
-                selected_samples = data.sample(n=5) ## few shot with 5 random drawn sentences
-                for i, row in selected_samples.iterrows():
-                    prompt_texts.append(template_few_shot.format(text,translate) for text, translate in zip(selected_samples[args.input_lang], selected_samples[args.output_lang]))
-                # prompt_texts = [template_few_shot.format(text, translate) for text, translate in zip(selected_samples[args.input_lang], selected_samples[args.output_lang])]
+        # for i in tqdm(range(0, len(input_texts), batch_size), total=total_batches, desc="Inference"):
+        if use_few_shot_prompting: ## few shot
+            for i in tqdm(range(0, len(input_texts)), total=len(input_texts), desc="Inference"):
+                prompt_texts = [f"Given the examples, translate the sixth {args.input_lang} sentence into {args.output_lang}.\n"]
+                # prompt_texts = ["把普通话句子翻译成粤语。"] ## translate mandarin to cantonese
+                # prompt_texts = ["把粵語句子翻譯成國語。"] ## translate cantonese to mandarin
+                prompt_texts.extend([template_few_shot.format(text, translate) for text, translate in zip(selected_samples[args.input_lang], selected_samples[args.output_lang])])
+                # print('prompt_texts:', prompt_texts)
+                next_line = [f'{args.input_lang}: '+input_texts[i]+'\n'+f'{args.output_lang}:']
+                prompt_texts.extend(next_line)
+                
                 inputs = tokenizer(prompt_texts, return_tensors="pt", padding=True, truncation=True, max_length=512).to('cuda')
-            
-            ## zero shot
-            ## input is 
-            else:
+                generate_ids = model.generate(**inputs, max_new_tokens=260)
+                batch_outputs = tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)
+
+                # import pudb; pudb.set_trace()
+                result = [batch_outputs[-1].split(f'{args.output_lang}:')[-1].strip()]
+                outputs.extend(result)
+                # print(outputs)
+
+        else: ## zero shot
+            for i in tqdm(range(0, len(input_texts), batch_size), total=total_batches, desc="Inference"):
+                batch_texts = input_texts[i:i+batch_size]
                 inputs = tokenizer([template_zero_shot.format(text, '') for text in batch_texts], return_tensors="pt", padding=True, truncation=True, max_length=512).to('cuda')
 
-            # Generate
-            generate_ids = model.generate(**inputs, max_new_tokens=512)
-            batch_outputs = tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)
-            #print(batch_outputs[1].split(f'{args.output_lang}'))
-            # import pudb; pudb.set_trace()
-            # for out in batch_outputs:
-            #     print('out', out.split('mandarin'))
-            batch_outputs = [out.split(f'{args.output_lang}:')[1].strip() for out in batch_outputs]
+                # Generate
+                generate_ids = model.generate(**inputs, max_new_tokens=260)
+                batch_outputs = tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)
 
-            outputs.extend(batch_outputs)
-        
+                # import pudb; pudb.set_trace()
+                batch_outputs = [out.split(f'{args.output_lang}:')[1].strip() for out in batch_outputs] ##[:1] to view the first row
+                # print(batch_outputs)
+            
+                outputs.extend(batch_outputs)
         return outputs
 
     def post_processing(input_text):
